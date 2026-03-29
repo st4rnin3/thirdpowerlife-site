855f1b8d143e7a805c34e2078830d74c0bb04460
st4rnin3 <st4rnin3@users.noreply.github.com>
2026-03-29 18:24:27 +0000

    feat: add Facebook Pixel (193078867851800)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import Footer from "@/components/Footer";
 import "./globals.css";
 
 const GA_ID = "G-7189T07H60";
+const FB_PIXEL_ID = "193078867851800";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -55,6 +56,31 @@ export default function RootLayout({
             gtag('config', '${GA_ID}');
           `}
         </Script>
+
+        {/* Facebook Pixel */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${FB_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
       </head>
       <body className="antialiased bg-navy text-light font-body">
         <Header />
